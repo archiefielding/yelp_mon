259ae106783ed8ce0885663d954e60dc706fcd3c
Archie Fielding <archiefielding@gmail.com>
2015-10-12 15:07:40 +0100

    added form for submitting new restaurant

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -5,7 +5,30 @@ feature 'restaurants' do
     scenario 'should display a prompt to add a restaurant' do
       visit '/restaurants'
       expect(page).to have_content 'No restaurants yet'
-      expect(page).to have_link 'Add a restaurants'
+      expect(page).to have_link 'Add a restaurant'
+    end
+  end
+
+  context 'restaurants have been added' do
+    before do
+      Restaurant.create(name: 'Chipotle')
+    end
+
+    scenario 'dsiplay restaurants' do
+      visit '/restaurants'
+      expect(page).to have_content('Chipotle')
+      expect(page).not_to have_content('No restaurants yet')
+    end
+  end
+
+  context 'creating restaurants' do
+    scenario 'prompts user to fill out a form, then displays the new restaurant' do
+      visit '/restaurants'
+      click_link 'Add a restaurant'
+      fill_in 'Name', with: 'Chipotle'
+      click_button 'Create Restaurant'
+      expect(page).to have_content 'Chipotle'
+      expect(current_path).to eq '/restaurants'
     end
   end
 end
